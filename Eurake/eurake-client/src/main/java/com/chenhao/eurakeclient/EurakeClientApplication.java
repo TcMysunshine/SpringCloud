955f61e1786579561ff4a18b1e4752578fa9b743
TcMysunshine <2229238008@qq.com>
2018-11-29 14:50:53 +0800

    Eureka Demo

---
+++ Eurake/eurake-client/src/main/java/com/chenhao/eurakeclient/EurakeClientApplication.java
@@ -0,0 +1,14 @@
+package com.chenhao.eurakeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurakeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakeClientApplication.class, args);
+	}
+}
